d2deada8d247c4dfc1b8a462762fc97dccc667e1
Huerto Recofsky <maria.recofsky@mercadolibre.com>
2020-12-10 15:23:29 -0300

    se cambio menor o igual 4

--- src/main/java/ar/edu/ge/meli/AsignadorDeVehiculos.java
+++ src/main/java/ar/edu/ge/meli/AsignadorDeVehiculos.java
@@ -3,7 +3,7 @@ package ar.edu.ge.meli;
 public class AsignadorDeVehiculos {
     public Vehiculo asignarVehiculo(Envio envio) throws EsMuyPesadoException {
         Double peso = envio.calcularPeso();
-        if(peso < 5) {
+        if(peso <= 4) {
             return Vehiculo.BICICLETA;
         } else if (peso < 50) {
             return Vehiculo.AUTO;
